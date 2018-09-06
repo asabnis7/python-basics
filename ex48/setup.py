6bc863c97b048add86a45785a456df2ab20a5715
Arjun Sabnis <arjun.sabnis@gatech.edu>
2018-09-05 20:14:53 -0400

    Add files via upload

--- ex48/setup.py
+++ ex48/setup.py
@@ -1,1 +1,19 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+	
+config = {
+	'description': 'My Project',
+	'author':'Arjun Sabnis',
+	'url':'URL to get it at',
+	'download_url':'Where to download',
+	'author_email':'arjunsabnis@gmail.com',
+	'version':'0.1',
+	'install_requires': ['nose'],
+	'packages':['ex48'],
+	'scripts':[],
+	'name':'Exercise 48'
+}
 
+setup(**config)
